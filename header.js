ca70729cf19e1160c6e31837795e6c30ae45c959
ritullia <brigrita@gmail.com>
2023-02-14 21:38:25 +0200

    header.js commit

---
+++ header.js
@@ -0,0 +1,7 @@
+export default function appHeader() {
+
+    let app = document.createElement('header');
+    app.innerHTML = 'Cia header';
+    document.body.append(app)
+
+}
--- sripts.js
+++ sripts.js
@@ -1,6 +1,7 @@
 
 import { numberSquare } from "./math.js"
 import getNumbers from "./multi.js"
+import appHeader from "./header.js"
 
 document.querySelector('button').addEventListener('click', () => {
     console.log('papsausta')
@@ -8,9 +9,11 @@ document.querySelector('button').addEventListener('click', () => {
     alert(number)
 });
 
-
+appHeader()
 let getNumber = getNumbers(2, 8)
 console.log(getNumber)
 
 
 //Sukurkite modulį (t.y. file'ą - multi.js), kuriame būtų viena eksportuota (default) funkcija - multiplikacija. Ji priims du skaičius kaip parametrus ir grąžins šiuos skaičius sudaugintus. Pagrindiniame JS file'e (script.js) importuokite ir panaudokite šį modulį.
+//Sukurkite modulį pavadinimu header.js. Šiame modulyje bus funkcija, kuri paims app kaip argumentą, tačiau nieko negrąžins. Šios funkcijos tikslas - į app.innerHTML pridėti (t.y. +=) header'io kodą (pasirašykite standartinį HTML su inline CSS). Šį modulį importuokite į pagrindinį file'ą ir paduokite app, kuris bus document.body. Taip, puslapio viršuje turėtų atsivaizduoti header'is, kuris importuotas iš header.js.
+//Įkelti šitą kodą į github repositoriją, sukurti github Pages ir įkelti link'ą į chat'ą
